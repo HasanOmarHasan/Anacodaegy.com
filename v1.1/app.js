1bb13762a4fd1cc8f1a535f1898eade46189f098
Hasan Omar <100947729+HasanOmarHasan@users.noreply.github.com>
2023-03-09 16:26:26 +0200

    updata "add sticky "

---
+++ v1.1/app.js
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////
+// Sticky Navigation
+
+// const sectionHeroEL = document.querySelector(".container ");
+
+// const obs = new IntersectionObserver(function (entries) {
+//     const ent = entries[0];
+//     console.log(ent);
+//     if (ent.isIntersecting === false) {
+//         document.body.classList.add('sticky')
+//     }
+//     if (ent.isIntersecting) {
+//         document.body.classList.remove('sticky')
+//     }
+// },
+//     {
+//         root: null, threshold: 0, rootMargin: "-80px "
+//     });
+// obs.observe(sectionHeroEL);
